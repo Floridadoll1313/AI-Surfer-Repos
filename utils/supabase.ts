69f9f27ec5ab273cdf7ad6d65c834ec5f4e72ace
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-06-06 17:36:31 -0400

    Create utils/supabase.ts

---
+++ utils/supabase.ts
@@ -0,0 +1,6 @@
+1import { createClient } from '@supabase/supabase-js';
+2
+3const supabaseUrl =https://mkgnyarwiscttobnytin.supabase.co import.meta.env.VITE_SUPABASE_URL;
+4const supabaseKey = NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_Jp0Laxs-KoieNMD5hqLA0w_jCnrxATm import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+5
+6export const supabase = createClient(
