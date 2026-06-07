292d241ad23b73ea14fb1d8214e14deaa77bbd37
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-06-07 16:31:49 -0400

    Update githubService.js

--- services/githubService.js
+++ services/githubService.js
@@ -2,10 +2,10 @@ const GITHUB_USER = "Floridadoll1313";
 
 export async function fetchRepos() {
   const res = await fetch(
-    `https://api.github.com/users/${GITHUB_USER}/repos?per_page=100`
+    `https://api.github.com/users/${GITHUB_USER}/repos?per_page=100&sort=updated`
   );
 
-  if (!res.ok) throw new Error("Failed to fetch repos");
+  if (!res.ok) throw new Error("GitHub fetch failed");
 
   return await res.json();
 }
